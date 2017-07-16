bc1ada963d9b1bf2f772ae977b989d441979f82c
Sirisak.J <ics.meff@gmail.com>
2017-07-16 12:01:28 +0700

    Api Test added

--- app/src/androidTest/java/com/sirisakj/technews/ApiTest.java
+++ app/src/androidTest/java/com/sirisakj/technews/ApiTest.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.sirisakj.technews.rest.ApiManager;
+import com.sirisakj.technews.rest.newsapi.model.response.ArticleListResponse;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import retrofit2.Response;
 
 import static org.junit.Assert.*;
 
@@ -15,12 +20,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ApiTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sirisakj.technews", appContext.getPackageName());
+        // Test Api
+        Response<ArticleListResponse> res = ApiManager.getInstance().newsApi.getArticlesBySource("techcrunch").execute();
+        assertTrue("Call newsapi techcrunch is successul", res.isSuccessful());
+        assertTrue("techcrunch-body is status ok", res.body().getStatus().equals("ok"));
+        assertNotNull("techcrunch-body articles not null", res.body().getArticles());
     }
 }
